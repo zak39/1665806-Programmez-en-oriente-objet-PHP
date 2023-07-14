ea057dba2e4e8356331cb84d36466b56499a7dee
zak39 <fotia.baptiste@hotmail.com>
2023-07-14 20:14:32 +0200

    Ma correction
    Si besoin, voir la branche du remote upstream :
    https://github.com/OpenClassrooms-Student-Center/1665806-Programmez-en-oriente-objet-PHP/tree/p3c5-correction
    .

--- src/MatchMaker/Player/QueuingPlayer.php
+++ src/MatchMaker/Player/QueuingPlayer.php
@@ -13,11 +13,12 @@ declare(strict_types=1);
 
 namespace App\MatchMaker\Player;
 
-class QueuingPlayer extends Player implements InLobbyPlayerInterface
+class QueuingPlayer implements InLobbyPlayerInterface
 {
-    public function __construct(PlayerInterface $player, protected int $range = 1)
+    protected int $range = 1;
+
+    public function __construct(protected PlayerInterface $player)
     {
-        parent::__construct($player->getName(), $player->getRatio());
     }
 
     public function getRange(): int
@@ -28,5 +29,25 @@ class QueuingPlayer extends Player implements InLobbyPlayerInterface
     public function upgradeRange(): void
     {
         $this->range = min($this->range + 1, 40);
+    }
+
+    public function getName(): string
+    {
+        return $this->player->getName();
+    }
+
+    public function getRatio(): float
+    {
+        return $this->player->getRatio();
+    }
+
+    public function getPlayer(): PlayerInterface
+    {
+        return $this->player;
+    }
+
+    public function updateRatioAgainst(PlayerInterface $player, int $result): void 
+    {
+        $this->player->updateRatioAgainst($player, $result);
     }
 }
